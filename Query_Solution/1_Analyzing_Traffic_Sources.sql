8593037ed8d039adfe830573d419a2d6621bd986
Faiz Naida <115857221+faizns@users.noreply.github.com>
2023-01-29 17:59:45 +0700

    Update 1_Analyzing_Traffic_Sources.sql

--- Query_Solution/1_Analyzing_Traffic_Sources.sql
+++ Query_Solution/1_Analyzing_Traffic_Sources.sql
@@ -17,40 +17,40 @@ ORDER BY 4 DESC
 
 -- 2. Traffic Conversion Rate
 SELECT
-    COUNT(DISTINCT w.website_session_id) AS sessions,
-    COUNT(DISTINCT o.order_id) AS orders,
-    COUNT(DISTINCT o.order_id)/COUNT(DISTINCT w.website_session_id) AS session_to_order_CVR
+	COUNT(DISTINCT w.website_session_id) AS sessions,
+	COUNT(DISTINCT o.order_id) AS orders,
+	COUNT(DISTINCT o.order_id)/COUNT(DISTINCT w.website_session_id) AS session_to_order_CVR
 FROM website_sessions w
 	LEFT JOIN orders o
 		ON o.website_session_id = w.website_session_id
 WHERE w.created_at < '2012-04-14'
 	AND utm_source = 'gsearch'
-    AND utm_campaign = 'nonbrand'
+	AND utm_campaign = 'nonbrand'
 
 
 -- 3. Traffic Source Trending
 SELECT
 	MIN(DATE(created_at)) AS week_start_date,
-    COUNT(DISTINCT website_session_id) AS sessions
+	COUNT(DISTINCT website_session_id) AS sessions
 FROM website_sessions 
 WHERE created_at < '2012-05-10'
 	AND utm_source = 'gsearch'
-    AND utm_campaign = 'nonbrand'
+	AND utm_campaign = 'nonbrand'
 GROUP BY YEAR(created_at), WEEK(created_at)
 
 
 -- 4. Traffic Source Bid Optimization
 SELECT
 	w.device_type,
-    COUNT(DISTINCT w.website_session_id) AS sessions,
+	COUNT(DISTINCT w.website_session_id) AS sessions,
 	COUNT(DISTINCT o.order_id) AS orders,
-    COUNT(DISTINCT o.order_id)/COUNT(DISTINCT w.website_session_id) AS session_to_order_CVR
+	COUNT(DISTINCT o.order_id)/COUNT(DISTINCT w.website_session_id) AS session_to_order_CVR
 FROM website_sessions w
 	LEFT JOIN orders o
 		ON o.website_session_id = w.website_session_id
 WHERE w.created_at < '2012-05-11'
 	AND w.utm_source = 'gsearch'
-    AND w.utm_campaign = 'nonbrand'
+	AND w.utm_campaign = 'nonbrand'
 GROUP BY 1
 ORDER BY 4 DESC
 
@@ -58,11 +58,11 @@ ORDER BY 4 DESC
 -- 5. Traffic Source Segment Trending
 SELECT
 	MIN(DATE(created_at)) AS week_start_date,
-   	COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS dtop_session,
-   	COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mob_session
+	COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS dtop_session,
+	COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mob_session
 FROM website_sessions
 WHERE created_at < '2012-06-09'
 	AND created_at > '2012-04-15'
 	AND utm_source = 'gsearch'
-   	AND utm_campaign = 'nonbrand'
+	AND utm_campaign = 'nonbrand'
 GROUP BY YEAR(created_at), WEEK(created_at)
