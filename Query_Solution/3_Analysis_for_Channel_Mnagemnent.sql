3ae32d8c565890dc360763a2350817b1de21c57b
Faiz Naida <115857221+faizns@users.noreply.github.com>
2023-03-08 08:12:43 +0700

    Update and rename Analysis_for_Channel_Mnagemnent.sql to 3_Analysis_for_Channel_Mnagemnent.sql

--- Query_Solution/3_Analysis_for_Channel_Mnagemnent.sql
+++ Query_Solution/3_Analysis_for_Channel_Mnagemnent.sql
@@ -51,21 +51,20 @@ GROUP BY 1,2;
 
 -- 4. Channel Portfolio Trends
 SELECT
-    MIN(DATE(created_at)) AS week_start_date,
-    COUNT(DISTINCT website_session_id) AS total_session,
-    COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS gd_session,
-    COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS bd_session,
-    ROUND(100*(COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END)/
+	MIN(DATE(created_at)) AS week_start_date,
+	COUNT(DISTINCT website_session_id) AS total_session,
+	COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS gd_session,
+	COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END) AS bd_session,
+	ROUND(100*(COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END)/
 		COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END)),2) AS percentage_gd_bd,
-    
-    COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS gm_session,
-    COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS bm_session,
+	COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS gm_session,
+	COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'mobile' THEN website_session_id ELSE NULL END) AS bm_session,
 	ROUND(100*(COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END)/
 		COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND device_type = 'desktop' THEN website_session_id ELSE NULL END)),2) AS percentage_gm_bm
 FROM website_sessions
 WHERE created_at BETWEEN '2012-11-04' AND '2012-12-22'
-    AND utm_source IN ('gsearch', 'bsearch')
-    AND utm_campaign = 'nonbrand'
+	AND utm_source IN ('gsearch', 'bsearch')
+	AND utm_campaign = 'nonbrand'
 GROUP BY WEEK(created_at);
 
 
@@ -73,13 +72,13 @@ GROUP BY WEEK(created_at);
 SELECT DISTINCT
 	CASE 
 		WHEN utm_source IS NULL AND http_referer IN ('https://www.gsearch.com', 'https://www.bsearch.com') THEN 'organic_search'
-        WHEN utm_campaign = 'brand' THEN 'paid_brand'
-        WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
-        WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
+		WHEN utm_campaign = 'brand' THEN 'paid_brand'
+		WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
+		WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
 	END AS channel_group,
-    utm_source,
-    utm_campaign,
-    http_referer
+	utm_source,
+	utm_campaign,
+	http_referer
 FROM website_sessions
 WHERE created_at < '2012-12-23';
 
@@ -88,29 +87,29 @@ WHERE created_at < '2012-12-23';
 WITH channel_cte AS(
 SELECT 
 	created_at,
-    website_session_id,
+	website_session_id,
 	CASE 
 		WHEN utm_source IS NULL AND http_referer IN ('https://www.gsearch.com', 'https://www.bsearch.com') THEN 'organic_search'
-        WHEN utm_campaign = 'brand' THEN 'paid_brand'
-        WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
-        WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
+		WHEN utm_campaign = 'brand' THEN 'paid_brand'
+		WHEN utm_campaign = 'nonbrand' THEN 'paid_nonbrand'
+		WHEN utm_source IS NULL AND http_referer IS NULL THEN 'direct_type_in'
 	END AS channel_group
 FROM website_sessions
 WHERE created_at < '2012-12-23')
 
 SELECT 
 	MONTH(created_at) AS months,
-    COUNT(DISTINCT CASE WHEN channel_group = 'paid_brand' THEN website_session_id ELSE NULL END) AS paid_brand,
-    COUNT(DISTINCT CASE WHEN channel_group = 'paid_nonbrand' THEN website_session_id ELSE NULL END) AS paid_nonbrand,
-    ROUND(100*(COUNT(DISTINCT CASE WHEN channel_group = 'paid_brand' THEN website_session_id ELSE NULL END)/
+	COUNT(DISTINCT CASE WHEN channel_group = 'paid_brand' THEN website_session_id ELSE NULL END) AS paid_brand,
+	COUNT(DISTINCT CASE WHEN channel_group = 'paid_nonbrand' THEN website_session_id ELSE NULL END) AS paid_nonbrand,
+	ROUND(100*(COUNT(DISTINCT CASE WHEN channel_group = 'paid_brand' THEN website_session_id ELSE NULL END)/
 		COUNT(DISTINCT CASE WHEN channel_group = 'paid_nonbrand' THEN website_session_id ELSE NULL END)),2) AS percent_ratio_brand_nonbrand,
-    
-    COUNT(DISTINCT CASE WHEN channel_group = 'organic_search' THEN website_session_id ELSE NULL END) AS organic_search,
-    ROUND(100*(COUNT(DISTINCT CASE WHEN channel_group = 'organic_search' THEN website_session_id ELSE NULL END)/
+
+	COUNT(DISTINCT CASE WHEN channel_group = 'organic_search' THEN website_session_id ELSE NULL END) AS organic_search,
+	ROUND(100*(COUNT(DISTINCT CASE WHEN channel_group = 'organic_search' THEN website_session_id ELSE NULL END)/
 		COUNT(DISTINCT CASE WHEN channel_group = 'paid_nonbrand' THEN website_session_id ELSE NULL END)),2) AS percent_ratio_organic_nonbrand,
-    
-    COUNT(DISTINCT CASE WHEN channel_group = 'direct_type_in' THEN website_session_id ELSE NULL END) AS direct,
-    ROUND(100*(COUNT(DISTINCT CASE WHEN channel_group = 'direct_type_in' THEN website_session_id ELSE NULL END)/
+
+	COUNT(DISTINCT CASE WHEN channel_group = 'direct_type_in' THEN website_session_id ELSE NULL END) AS direct,
+	ROUND(100*(COUNT(DISTINCT CASE WHEN channel_group = 'direct_type_in' THEN website_session_id ELSE NULL END)/
 		COUNT(DISTINCT CASE WHEN channel_group = 'paid_nonbrand' THEN website_session_id ELSE NULL END)),2) AS percent_ratio_direct_nonbrand
 FROM channel_cte
 GROUP BY MONTH(created_at) 
